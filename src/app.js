ec377a920e5c71d12a808a81d9fa426698e178cc
Paola Chávez <56763235+Anacronica@users.noreply.github.com>
2024-05-18 20:28:22 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ const adjectives = ["great", "big"];
 const nouns = ["jogger", "racoon"];
 const dominios = [".net", ".com", ".es"];
 
-pronouns.forEach(pronoun => {
-  adjectives.forEach(adjective => {
-    nouns.forEach(noun => {
-      dominios.forEach(domi => {
-        //console.log(pronoun + adjective + noun + domi);
+pronouns.forEach((pronoun) => {
+  adjectives.forEach((adjective) => {
+    nouns.forEach((noun) => {
+      dominios.forEach((domi) => {
+        console.log(pronoun + adjective + noun + domi);
       });
     });
   });
